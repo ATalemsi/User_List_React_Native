eec7d7f668f0b209cd74caa9147cac4b0d3d40f4
ATalemsi <mohamadtalemsi@gmail.com>
2024-05-15 16:14:30 +0100

    first-commit

--- App.js
+++ App.js
@@ -1,21 +1,72 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, FlatList, ActivityIndicator, StyleSheet } from 'react-native';
+import axios from 'axios';
 
-export default function App() {
+const App = () => {
+  const [loading, setLoading] = useState(true);
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await axios.get('https://61m2wrb52b.execute-api.eu-north-1.amazonaws.com/dev/media');
+        const userList = response.data;
+        setUsers(userList);
+        setLoading(false); 
+      } catch (error) {
+        console.error('Error fetching users:', error);
+        setLoading(false); 
+      }
+    };
+
+    fetchUsers();
+  }, []); 
+
+  if (loading) {
+    return (
+      <View style={styles.loadingContainer}>
+        <ActivityIndicator size="large" color="#0000ff" />
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>List of Users:</Text>
+      <FlatList
+        data={users}
+        keyExtractor={(item) => item.user_id.toString()}
+        renderItem={({ item }) => (
+          <View style={styles.userItem}>
+            <Text>{item.full_name} ------------------------------------------------------------------- with username :{item.username}</Text>
+          </View>
+        )}
+      />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    paddingTop: 20,
+    paddingHorizontal: 10,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  userItem: {
+    paddingVertical: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+  },
+  loadingContainer: {
+    flex: 1,
     justifyContent: 'center',
+    alignItems: 'center',
   },
 });
+
+export default App;
